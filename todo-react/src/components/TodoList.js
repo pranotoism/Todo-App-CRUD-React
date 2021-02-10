260318942e9c5ec1ed83918d64f81afefa07ed62
pranotoism <bayupranotodwiputra@gmail.com>
2021-02-10 07:52:11 +0700

    sort todo list by priority

--- todo-react/src/components/TodoList.js
+++ todo-react/src/components/TodoList.js
@@ -5,7 +5,7 @@ import Todo from './Todo'
 function TodoList({ todos, toggleComplete, removeTodo }) {
   return (
     <List>
-      {todos.map(todo => (
+      { todos.sort((a,b) => (a.priority < b.priority) ? 1 : ((b.priority < a.priority) ? -1 : 0)).map(todo => (
         <Todo
           key={todo.id}
           todo={todo}
